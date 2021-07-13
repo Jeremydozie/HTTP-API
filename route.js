69be577a27fe76fa9f6c4cf0b7c1f3bce04689b3
Jeremydozie <65295271+Jeremydozie@users.noreply.github.com>
2021-07-12 20:39:19 -0500

    Create route.js

---
+++ route.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var sql = require("mssql");
+var conn = require("../connection/connect")();
+
+.get(function (req, res)
+
+conn.connect().then(function ()
+           {
+
+var sqlQuery = "SELECT * FROM Products";
+
+var req = new sql.Request(conn);
+               req.query(sqlQuery).then(function (recordset)
+               {
+                   res.json(recordset.recordset);
+                   conn.close();
+               })
+
+.catch(function (err) {
+                       conn.close();
+                       res.status(400).send("Error while inserting data");
+                   });
